31b489cd59b4c4e656b410e125f238b7492a5427
gabrieledaronco <gabrieledaronco@gmail.com>
2023-10-30 00:47:26 +0100

    first-release

--- sections_EC2_app.py
+++ sections_EC2_app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import sections_EC2_module as sm
+from io import BytesIO
+import numpy as np
+## Import analysis section
+from concreteproperties.concrete_section import ConcreteSection
+from math import pi
+
+
+st.header("Reinforced Concrete Sections")
+st.subheader("Rectangular sections")
 
 col1, col2 =st.columns(2)
-with col1:
-    st.header("Reinforced Concrete Sections")
-    st.subheader("Rectangular sections")
-
-with col2:
-    st. write("Hola")
+tab1, tab2, tab3 = st.tabs(["Geometry","M-N Results", "Cracking"])
 
 st.sidebar.subheader("Concrete")
 concrete = st.sidebar.text_input("Concrete grade", value="C25/30")
@@ -25,28 +29,113 @@ concrete = sm.create_concrete(fc=c_fc,fc_t=c_fct,E=c_E)
 steel_rebar=sm.create_steelbar(fy=s_fy,gamma_r=1.15)
 
 st.sidebar.subheader("Section Geometry")
+type = st.sidebar.text_input("Section Type", value="Rectangular")
 s_h = st.sidebar.number_input("height [mm]", value=900)
 s_b = st.sidebar.number_input("width [mm]", value=600)
 
+st.sidebar.subheader("Reinforcement Bars")
+t_diameter= st.sidebar.number_input("Top diameter [mm]")
+t_nr_bars= st.sidebar.number_input("Top number of bars", format="%i" )
+b_diameter= st.sidebar.number_input("Bottom diameter [mm]")
+b_nr_bars= st.sidebar.number_input("Bottom number of bars", format="%i")
+l_diameter= st.sidebar.number_input("Left diameter [mm]")
+l_nr_bars= st.sidebar.number_input("Left number of bars", format="%i" )
+r_diameter= st.sidebar.number_input("Right diameter [mm]")
+r_nr_bars= st.sidebar.number_input("Right number of bars", format="%i" )
+cover = st.sidebar.number_input("Cover [mm]")
+
+st.sidebar.subheader("Acions")
+n_action= st.sidebar.number_input("ULS Axial_force [kN]")
+m_action= st.sidebar.number_input("ULS Bending Moment [kNm]")
+sls_n_action= st.sidebar.number_input("SLS Axial_force [kN]")
+sls_m_action= st.sidebar.number_input("SLS Bending Moment [kNm]")
+
+
+
+#Define concrete geometry
 conc_geom = sm.def_geom(height=s_h,width=s_b, mat=concrete)
 
-bars = [
-[500.000000,-250,384.824982],
-[500.000000,250,384.824982],
-[500.000000,-250,-384.824982],
-[500.000000,250,-384.824982],
-[500.000000,-250,230.894989],
-[500.000000,-250,76.964996],
-[500.000000,-250,-76.964996],
-[500.000000,-250,-230.894989],
-[500.000000,250,230.894989],
-[500.000000,250,76.964996],
-[500.000000,250,-76.964996],
-[500.000000,250,-230.894989],
-[500.000000,0,384.824982],
-[500.000000,0,-384.824982],
-]
+bars_list = sm.rect_bar_list(top_diameter=t_diameter,
+                             top_nr_bars=int(t_nr_bars),
+                             bottom_diameter=b_diameter,
+                             bottom_nr_bars=int(b_nr_bars),
+                             left_diameter=l_diameter,
+                             left_nr_bars=int(l_nr_bars),
+                             right_diameter=r_diameter,
+                             right_nr_bars=int(r_nr_bars),
+                             height=s_h,
+                             width=s_b,
+                             cover=cover
+                             )
 
-reinf_geom = sm.add_bars(bars_list=bars,conc_geom=conc_geom,mat=steel_rebar)
+conc_geom = sm.add_bars(bars_list=bars_list,conc_geom=conc_geom,mat=steel_rebar)
+#Define concrete section
+conc_section = ConcreteSection(conc_geom)
 
 
+
+
+#Actions
+actions_dict = {"L1":(n_action,m_action),}
+actions=list(actions_dict.values())
+actions_ordered=list(zip(*actions))
+
+#Caclutate Mr for each N (CREARE TABELLA PER METTERE I VALORI)
+capacity_moments={}
+for name,actions in actions_dict.items():
+    mr = conc_section.ultimate_bending_capacity(theta=0, n= actions[0]*1e3)
+    capacity_moments.update({name:mr.m_xy/1e6})
+
+#Calculate Cracked Section
+cracked_res = conc_section.calculate_cracked_properties(theta=0)
+cracked_stress_res = conc_section.calculate_cracked_stress(
+    cracked_results=cracked_res,n= sls_n_action, m=sls_m_action
+)
+
+#print N-M curve with actions
+m_n=conc_section.moment_interaction_diagram(theta=0)
+m_n_list=m_n.get_results_lists("m_xy")
+# m_n_180=conc_section.moment_interaction_diagram(theta=pi)
+# m_n_0_list=m_n_0.get_results_lists("m_xy")
+# m_n_180_list=m_n_180.get_results_lists("m_xy")
+# m_n_list_n = m_n_0_list[0]+ m_n_180_list[0]
+# m_n_list_m = m_n_0_list[1]+ m_n_180_list[1]
+# m_n_list= (m_n_list_n,m_n_list_m)
+m_n_array =np.array(m_n_list)
+
+#Tabs display:
+
+with tab1:
+    fig = Figure()
+    ax = fig.gca()
+    ax = conc_section.plot_section()
+    fig=ax.get_figure()
+
+    temp_fig= BytesIO()
+    fig.savefig(temp_fig, format="png")
+    st.image(temp_fig)
+
+with tab2:
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(name= "Section Capacity",y=m_n_array[1]/1e6, x=m_n_array[0]/1e3))
+    fig.add_trace(go.Scatter(name= "External action",x=actions_ordered[0], y=actions_ordered[1], mode="markers"))
+    fig.layout.width = 800
+    fig.layout.height = 800
+    fig.layout.xaxis.title = "Axial Force (kN)"
+    fig.layout.yaxis.title = "Bending Moment (kNm)"
+    fig
+    for lc,mr in capacity_moments.items():
+        st.write(f"The Bending Capacity is equal to {mr.round(1)} kNm")
+
+with tab3:
+    fig_2 = Figure()
+    ax_2 = fig_2.gca()
+    ax_2 = cracked_stress_res.plot_stress()
+
+    fig_2=ax_2.get_figure()
+
+    temp_fig_2= BytesIO()
+    fig_2.savefig(temp_fig_2, format="png")
+    st.image(temp_fig_2)
+  
+    st.write(f"Depth of neutral axis is equal to {cracked_res.d_nc:.2f} mm")
--- sections_EC2_module.py
+++ sections_EC2_module.py
@@ -1,3 +1,7 @@
+#General import
+from math import pi
+import numpy as np
+
 # Import stress/strain profiles
 from concreteproperties.stress_strain_profile import (
     ConcreteLinearNoTension, 
@@ -32,7 +36,7 @@ def create_concrete(
     imported., accordin to EC2
     """
     concrete_service = ConcreteLinearNoTension(
-    elastic_modulus = E, # Use either Cl. 8.6.2.2 or Cl. 8.6.2.3
+    elastic_modulus = E,
     ultimate_strain = ult_strain,
     compressive_strength =  fc,
     )
@@ -85,9 +89,101 @@ def def_geom(height:float, width:float, mat:Concrete)-> Geometry:
 
 ##Add bars to section
 
-def add_bars(bars_list:list[list[float]], conc_geom:Geometry, mat:SteelBar):
-    for area,x,y in bars_list:
-        reinf_geometry = add_bar(conc_geom, area=area,material=mat, x=x,y=y)
-    return reinf_geometry
+def rect_bar_list(top_diameter:float, 
+             bottom_diameter:float, 
+             left_diameter:float,
+             right_diameter:float,
+             top_nr_bars:int,
+             bottom_nr_bars:int,
+             left_nr_bars:int,
+             right_nr_bars:int,
+             height:float,
+             width:float,
+             cover:float
+             )->list[list[float]]:
+    """
+    retunrs a list[list] with the coordinates and the anrea of the reinf bars to be used
+    assumptions: section has to be rectangular
+    """
+    area_top = (pi*top_diameter**2)/4
+    area_bottom = (pi*bottom_diameter**2)/4
+    area_left = (pi*left_diameter**2)/4
+    area_right = (pi*right_diameter**2)/4
+    s_top = (width-2*cover)/(top_nr_bars-1)
+    s_bottom = (width-2*cover)/(bottom_nr_bars-1)
+    s_left = (height-2*cover)/(left_nr_bars-1)
+    s_right = (height-2*cover)/(right_nr_bars-1)
+    top_list = []
+    bottom_list=[]
+    left_list=[]
+    right_list=[]
+    
+    for i in range(top_nr_bars):
+        if i ==0:
+            x=-width/2+cover
+            sub_list = [area_top, x, height/2-cover]
+        else:
+            x=x+s_top
+            sub_list = [area_top, x, height/2-cover]
+        top_list.append(sub_list)
+    
+    for i in range(bottom_nr_bars):
+        if i ==0:
+            x=-width/2+cover
+            sub_list = [area_bottom, x, -height/2+cover]
+        else:
+            x=x+s_bottom
+            sub_list = [area_bottom, x, -height/2+cover]
+        bottom_list.append(sub_list)
+
+    for i in range(left_nr_bars):
+        if area_left == 0:
+            continue
+        elif i ==0:
+            y=-height/2+cover
+            sub_list = [area_left, -width/2+cover, y]
+        else:
+            y=y+s_left
+            sub_list = [area_left, -width/2+cover, y]
+        left_list.append(sub_list)
+    
+    for i in range(right_nr_bars):
+        if area_right ==0:
+            continue
+        elif i ==0:
+            y=-height/2+cover
+            sub_list = [area_right, width/2-cover, y]
+        else:
+            y=y+s_right
+            sub_list = [area_right, width/2-cover, y]
+        right_list.append(sub_list)
+    
+    bar_list = top_list+bottom_list
+    if area_left ==0 or area_right ==0:
+        return bar_list
+    else:
+        for i in range(left_nr_bars):
+            if i ==0 or i == left_nr_bars-1:
+                continue
+            else:
+                bar_list.append(left_list[i])
+        for i in range(right_nr_bars):
+            if i ==0 or i == right_nr_bars-1:
+                continue
+            else:
+                bar_list.append(right_list[i])        
+        return bar_list
+
+def add_bars(bars_list:list[list[float]], conc_geom:Geometry, mat:SteelBar)->CompoundGeometry:
+    for rebar in bars_list:
+        area, x, y = rebar
+        conc_geom = add_bar(geometry=conc_geom, 
+                                 area=area,
+                                 material=mat, 
+                                 x=x,
+                                 y=y
+                                 )
+   
+    return conc_geom
 
 
